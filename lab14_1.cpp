e09cddd1d595a7bae0c23d90d0ff226d53a7b939
Your Name <kawin_chusin@cmu.ac.th>
2023-01-13 17:41:39 +0700

    ff

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,23 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+
+void stat(const double a[],int b,double c[]){
+    double d,f,g,h=1;
+    double k=a[0],j=a[0];
+    for(int i=0;i<b;i++) d +=a[i];
+    c[0]=d/b;
+    for(int i=0;i<b;i++) f +=pow(a[i]-c[0],2);
+    c[1]=sqrt(f/b);
+    for(int i=0;i<b;i++) g +=1/a[i];
+    c[3]=b/g;
+    for(int i=0;i<b;i++) h *=a[i];
+    c[2]=pow(h,(1.0/b));
+    for(int i=1;i<b;i++){
+        if(k<a[i]) k=a[i];
+        if(j>a[i]) j=a[i];
+    }
+    c[4]=k;
+    c[5]=j;
+}
